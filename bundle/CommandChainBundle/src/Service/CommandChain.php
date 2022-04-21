543a6b0fd174315cb676a8a39b0bc91f9a435346
Eugene Z <ezibrov@yahoo.com>
2022-04-21 12:51:34 +0300

    Refactoring

--- bundle/CommandChainBundle/src/Service/CommandChain.php
+++ bundle/CommandChainBundle/src/Service/CommandChain.php
@@ -3,27 +3,41 @@
 namespace Ezi\CommandChainBundle\Service;
 
 use Ezi\CommandChainBundle\Exception\CommandExecutionException;
-use Ezi\CommandChainBundle\Exception\UnsupportedTypeException;
-use PHPUnit\Util\Exception;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class CommandChain implements CommandChainInterface
 {
+    /**
+     * @var LoggerInterface
+     */
+    private LoggerInterface $logger;
+    /**
+     * @var array
+     */
     private array $commandQueue;
 
+    /**
+     * @param LoggerInterface $logger
+     */
     public function __construct(LoggerInterface $logger)
     {
+        $this->logger = $logger;
         $this->commandQueue = [];
     }
 
-    public function execute(InputInterface $input, OutputInterface $output): int
+    /**
+     * @param OutputInterface $output
+     * @return int
+     * @throws CommandExecutionException
+     */
+    public function execute(OutputInterface $output): int
     {
         $result = Command::SUCCESS;
-        foreach ($this as $command) {
-            $result = $command->execute($input, $output);
+        foreach ($this->commandQueue as $name => $arr) {
+            $result = $arr['command']?->execute($arr['args'], $output);
             if ($result !== Command::SUCCESS) {
                 throw new CommandExecutionException();
             }
@@ -32,64 +46,14 @@ class CommandChain implements CommandChainInterface
         return $result;
     }
 
-    public function offsetExists(mixed $offset): bool
-    {
-        return array_key_exists($offset, $this->commandQueue);
-    }
-
-    public function offsetGet(mixed $offset): mixed
-    {
-        $returnValue = null;
-        if ($this->offsetExists($offset)) {
-            $returnValue = $this->commandQueue[$offset];
-        }
-        return $returnValue;
-    }
-
     /**
-     * @param mixed $offset
-     * @param Command $value
-     * @return void
-     * @throws UnsupportedTypeException
+     * @param Command $command
+     * @param ArrayInput|null $args
+     * @return CommandChain
      */
-    public function offsetSet(mixed $offset, mixed $value): void
+    public function pushCommand(Command $command, ArrayInput $args = null): CommandChain
     {
-        if( !$value instanceof Command) {
-            throw new UnsupportedTypeException();
-        }
-        $this->commandQueue[$offset] = $value;
-    }
-
-    public function offsetUnset(mixed $offset): void
-    {
-        if(!$this->offsetExists($offset)) {
-            throw new \OutOfBoundsException('Key not exists');
-        }
-        unset($this->commandQueue[$offset]);
-    }
-
-    public function current(): mixed
-    {
-        return current($this->commandQueue);
-    }
-
-    public function next(): void
-    {
-        next($this->commandQueue);
-    }
-
-    public function key(): mixed
-    {
-        return key($this->commandQueue);
-    }
-
-    public function valid(): bool
-    {
-        return $this->offsetExists($this->key());
-    }
-
-    public function rewind(): void
-    {
-        reset($this->commandQueue);
+        $this->commandQueue[$command->getName()] = ['command' => $command, 'args' => $args];
+        return $this;
     }
 }
--- bundle/CommandChainBundle/src/Service/CommandChainInterface.php
+++ bundle/CommandChainBundle/src/Service/CommandChainInterface.php
@@ -2,10 +2,22 @@
 
 namespace Ezi\CommandChainBundle\Service;
 
-use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\OutputInterface;
 
-interface CommandChainInterface extends \ArrayAccess, \Iterator
+interface CommandChainInterface
 {
-    public function execute(InputInterface $input, OutputInterface $output): int;
+    /**
+     * @param OutputInterface $output
+     * @return int
+     */
+    public function execute(OutputInterface $output): int;
+
+    /**
+     * @param Command $command
+     * @param ArrayInput|null $input
+     * @return CommandChainInterface
+     */
+    public function pushCommand(Command $command, ArrayInput $input = null): CommandChainInterface;
 }
