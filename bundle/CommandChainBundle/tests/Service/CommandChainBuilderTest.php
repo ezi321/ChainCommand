4e1aea716090be2dcd00057ed6db36c280b2c888
Eugene Z <ezibrov@yahoo.com>
2022-04-21 12:53:05 +0300

    Add tests

---
+++ bundle/CommandChainBundle/tests/Service/CommandChainBuilderTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ezi\CommandChainBundle\Tests\Service;
+
+use PHPUnit\Framework\TestCase;
+
+class CommandChainBuilderTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        //$routerService = static::getContainer()->get('router');
+        //$myCustomService = static::getContainer()->get(CustomService::class);
+    }
+}
